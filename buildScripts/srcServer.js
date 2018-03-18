6e26ab5015ee2a8cbca2363501b79186bd93719e
dillonchristensen <Dillongc21@gmail.com>
2018-03-18 01:17:19 -0600

    Fetch & Some API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,6 +21,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  //hard coded for simplicity, pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 1, "firstName":"Dillon","lastName":"Christensen","email":"dillongc21@gmail.com"},
+    {"id": 1, "firstName":"Jane","lastName":"Doe","email":"janeldoe@gmail.com"}
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import './index.css';
+import {getUsers} from "./api/users.js";
+
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `<tr>\n      
+                    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>\n      
+                    <td>${user.id}</td>\n     
+                    <td>${user.firstName}</td>\n      
+                    <td>${user.lastName}</td>\n      
+                    <td>${user.email}</td>\n      
+                  </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+});
 
